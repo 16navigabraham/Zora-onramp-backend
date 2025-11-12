c822da762aa968eaa706a909af6a9ef7c364a7e4
Adebanjo Abraham.I <navigatorabraham17@gmail.com>
2025-11-12 14:22:33 +0100

    fix(telegram): add BalanceCheckService and ContractsModule to TelegramModule

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,11 +1,14 @@
 import { Module, Global } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 import { TelegramController } from './telegram.controller';
+import { ContractsModule } from '../contracts/contracts.module';
+import { BalanceCheckService } from '../maintenance/balance-check.service';
 
 @Global()
 @Module({
+  imports: [ContractsModule],
   controllers: [TelegramController],
-  providers: [TelegramService],
+  providers: [TelegramService, BalanceCheckService],
   exports: [TelegramService],
 })
 export class TelegramModule {}
